6d8bf6527162be3bd59cda8050f0b1206cb5785b
samuelalex93 <smk.tecinfo@gmail.com>
2025-01-30 21:07:56 +0000

    feat: add table structure

--- crud-angular/src/app/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses.component.ts
@@ -1,11 +1,21 @@
+import { Course } from './model/course';
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  courses: Course[] = [
+    {_id: "1", name: 'Angular', category: 'Frontend'},
+    {_id: "2", name: 'Java', category: 'Backend'},
+  ]
 
+  constructor() {
+  }
+
+  displayedColumns: string[] = ['name', 'category'];
 }
