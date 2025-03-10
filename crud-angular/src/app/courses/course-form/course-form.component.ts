14cba967c1b65b78214f68d246f0b539d09f9254
samuelalex93 <smk.tecinfo@gmail.com>
2025-03-10 22:10:49 +0000

    feat: add form

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 
 @Component({
   selector: 'app-course-form',
-  imports: [],
+  imports: [ReactiveFormsModule, AppMaterialModule],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss'
 })
 export class CourseFormComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
+    
+  }
+
+  
 }
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -4,8 +4,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  exports: [MatTableModule, MatToolbarModule, MatCardModule, MatProgressSpinnerModule, MatIconModule],
+  exports: [MatTableModule, MatToolbarModule, MatCardModule, MatProgressSpinnerModule, MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class AppMaterialModule { }
